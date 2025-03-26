983ce1bef3c714c3d3132d390e06032a0b9d6af5
fibich <fibich@atteq.com>
2025-03-26 21:58:56 +0100

    Added overrides for search

---
+++ .config/nvim/lua/plugins/snacks-override.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          -- files = { hidden = true, dirs = { ".", "~/.config/nvim/", "meme/src/app/generated/openapi/" } },
+          -- grep = { hidden = true, dirs = { ".", "meme/src/app/generated/openapi/" } },
+          buffers = { hidden = true },
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("snacks.picker").files({
+            -- Let fd read .gitignore by default
+            hidden = true,
+            dirs = { ".", "meme/src/app/generated/openapi/" },
+          })
+        end,
+        desc = "Find Files (Root Dir, include `generated`)",
+      },
+      {
+        "<leader>/",
+        function()
+          require("snacks.picker").grep({
+            hidden = true,
+            dirs = { ".", "meme/src/app/generated/openapi/" },
+          })
+        end,
+        desc = "Grep (Root Dir, include `generated`)",
+      },
+    },
+  },
+}
