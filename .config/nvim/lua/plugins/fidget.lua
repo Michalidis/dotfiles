c8f34c62ac236c15834835a6a9c786292dbf982d
fibich <fibich@atteq.com>
2026-02-10 15:15:26 +0100

    Added loading animation while receiving AI response

---
+++ .config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,12 @@
+return {
+  "j-hui/fidget.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- Notification window options
+    notification = {
+      window = {
+        winblend = 0, -- Transparent background
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,42 @@
+local spinner_frames = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+local spinner_index = 0
+local spinner_timer = nil
+local cc_loading = false
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "CodeCompanionRequestStarted",
+  callback = function()
+    cc_loading = true
+    spinner_index = 0
+    if spinner_timer then
+      spinner_timer:stop()
+      spinner_timer:close()
+    end
+    spinner_timer = vim.uv.new_timer()
+    spinner_timer:start(
+      0,
+      80,
+      vim.schedule_wrap(function()
+        spinner_index = (spinner_index % #spinner_frames) + 1
+        vim.cmd("redrawstatus")
+      end)
+    )
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "CodeCompanionRequestFinished",
+  callback = function()
+    cc_loading = false
+    if spinner_timer then
+      spinner_timer:stop()
+      spinner_timer:close()
+      spinner_timer = nil
+    end
+    vim.cmd("redrawstatus")
+  end,
+})
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -5,6 +44,15 @@ return {
     require("lualine").setup({
       sections = {
         lualine_x = {
+          {
+            function()
+              if cc_loading then
+                return spinner_frames[spinner_index] .. " CodeCompanion"
+              end
+              return ""
+            end,
+            color = { fg = "#f7c67f" },
+          },
           function()
             return vim.g.mcphub_status or ""
           end,
